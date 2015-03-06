db121cccb8c6c910a791697641e1198ea3516c96
Lomany <lomany9852@gmail.com>
2015-03-06 11:48:30 +0600

    score added

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour 
 {
@@ -9,9 +10,14 @@ public class GameController : MonoBehaviour
 	public float spawnWait;
 	public float startWait;
 	public float waveWait;
+	public Text scoreText;
+	private int score;
+
 
 	void Start()
 	{
+		score = 0;
+		UpdateScore ();
 		StartCoroutine(SpawnWaves());
 	}
 
@@ -29,5 +35,16 @@ public class GameController : MonoBehaviour
 			}
 			yield return new WaitForSeconds(waveWait);
 		}
+	}
+
+	public void AddScore(int newScoreValue)
+	{
+		score += newScoreValue;
+		UpdateScore ();
+	}
+
+	void UpdateScore()
+	{
+		scoreText.text = "Score: " + score;
 	}
 }
